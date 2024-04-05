b9169b6bec72e64e33a25b59e70385abe4df4d46
dorothyd147 <dcd128@scarletmail.rutgers.edu>
2024-04-04 20:23:08 -0400

    Duplicate Scenes
    Created new scene for golf

---
+++ Assets/02-Golf/__Scripts/CardProspector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//enum, defines variable type with prenamed values
+public enum eCardState
+{
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+
+public class CardProspector : Card //extension of card class
+{
+    [Header("Set Dynamically: CardProspector")]
+    public eCardState state = eCardState.drawpile;
+    public List<CardProspector> hiddenBy = new List<CardProspector>();//stores which cards keep this one face down
+    public int layoutID;//matches this card to to the XML tableau if it's a tableau card
+    public slotDef slotDef;
+
+    public override void OnMouseUpAsButton()
+    {
+        Prospector.S.CardClicked(this);
+        base.OnMouseUpAsButton();
+    }
+}
